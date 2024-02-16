a6c64aa5c921bd3db9e7ca0cff8d73e92955c3ec
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-02-17 00:21:41 +0100

    fixtures added

--- src/test/java/pl/bratek20/keyvaluestorage/api/KeyValueStorageTest.java
+++ src/test/java/pl/bratek20/keyvaluestorage/api/KeyValueStorageTest.java
@@ -2,44 +2,52 @@ package pl.bratek20.keyvaluestorage.api;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pl.bratek20.keyvaluestorage.fixtures.Assert;
+import pl.bratek20.keyvaluestorage.fixtures.Setup;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static pl.bratek20.keyvaluestorage.fixtures.Assert.assertKeyValuePairList;
+import static pl.bratek20.keyvaluestorage.fixtures.Assert.assertValue;
 
 public abstract class KeyValueStorageTest {
     protected abstract KeyValueStorage createStorage();
 
     private KeyValueStorage storage;
+    private Setup setup;
 
     @BeforeEach
     void beforeEach() {
         storage = createStorage();
+        setup = new Setup(storage);
     }
 
     @Test
     void shouldSetAndGetValue() {
-        Key key = new Key("key");
-        Value value = new Value("value");
+        setup.set(new Setup.SetArgs("key", "value"));
 
-        storage.set(key, value);
-        var result = storage.get(key);
+        var result = storage.get(new Key("key"));
 
-        assertEquals(value, result);
+        assertValue(result, "value");
     }
 
     @Test
     void shouldGetAll() {
-        Key key1 = new Key("key1");
-        Value value1 = new Value("value1");
-        Key key2 = new Key("key2");
-        Value value2 = new Value("value2");
-
-        storage.set(key1, value1);
-        storage.set(key2, value2);
+        setup.setMany(
+            List.of(
+                new Setup.SetArgs("key1", "value1"),
+                new Setup.SetArgs("key2", "value2")
+            )
+        );
 
         var result = storage.getAll();
 
-        assertEquals(2, result.size());
-        assertTrue(result.contains(new KeyValuePair(key1, value1)));
-        assertTrue(result.contains(new KeyValuePair(key2, value2)));
+        assertKeyValuePairList(
+            result,
+            List.of(
+                new Assert.ExpectedKeyValuePair("key1", "value1"),
+                new Assert.ExpectedKeyValuePair("key2", "value2")
+            )
+        );
     }
 }
