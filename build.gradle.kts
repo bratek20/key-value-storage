c46916b48b29166ae31dcce64eed2dbb77740169
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-03-30 20:55:37 +0100

    proto removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,8 @@
 plugins {
     alias(libs.plugins.bratek20.base.conventions)
-
-    alias(libs.plugins.protobuf)
 }
 
 dependencies {
     implementation(libs.bratek20.starter)
     testImplementation(testFixtures(libs.bratek20.starter))
-
-    implementation(libs.protobuf.java)
-}
-
-protobuf {
-    protoc {
-        artifact = libs.protobuf.protoc.get().toString()
-    }
 }
